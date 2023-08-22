8d5c853a33d9162c71ef51b025759e1bace04106
MubashirAhmad-ai <mubashir.ahmad171@gmail.com>
2023-08-22 13:43:33 +0100

    made so many changes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, File, HTTPException, UploadFile
 from pydantic import BaseModel
 from app.model.model import predict_pipeline, creation_times_file, creation_times
-from app.helper_functions import  delete_old_subdirectories
+from app.helper_functions import  delete_old_subdirectories, generate_item_description, delete_directory
 from app.speechToText_model import speechToText_model
 from app.model.model import __version__ as model_version
 from PIL import Image,UnidentifiedImageError
@@ -11,11 +11,13 @@ import librosa
 from fastapi.responses import FileResponse
 import base64
 import shutil
+from collections import defaultdict
 
 
 app = FastAPI(title='Link API')
 
 
+class_counts = defaultdict(int)
 class ImgIn(BaseModel):
     img: bytes
     
@@ -28,72 +30,172 @@ def home():
     return{"Health Check": "OK", "Model_version": model_version}
 
 def is_audio_file(filename):
-    audio_extensions = ['mp3', 'wav', 'flac', 'ogg'] 
+    
+    audio_extensions = ['mp3', 'wav', 'ogg'] 
     file_extension = filename.split('.')[-1].lower()
     return file_extension in audio_extensions
 
     
-@app.post("/predict", response_model=DictOut)
-async def upload_image(image_file: UploadFile = File(...), audio_file: UploadFile = File(None)):
-    global stored_image_path 
-    contents = await image_file.read()
-    register_heif_opener()    
+# @app.post("/predict", response_model=DictOut)
+# async def upload_image(image_file: UploadFile = File(...), audio_file: UploadFile = File(None)):
+#     global stored_image_path 
+#     contents = await image_file.read()
+#     register_heif_opener()    
 
-    try:
-        img = Image.open(io.BytesIO(contents)) 
-    except Exception as e:
-        image_file_error = True
-    else:
-        image_file_error = False
+#     try:
+#         img = Image.open(io.BytesIO(contents)) 
+#     except Exception as e:
+#         image_file_error = True
+#     else:
+#         image_file_error = False
 
     
-    if not image_file_error:
-        image_predictions, image_relative_path = predict_pipeline(img, contents)
-        delete_old_subdirectories(creation_times_file, creation_times)
-        if not image_predictions:
-            image_prediction_empty = True
-        else:
-            image_prediction_empty = False
+#     if not image_file_error:
+#         image_predictions, image_relative_path = predict_pipeline(img, contents)
+#         delete_old_subdirectories(creation_times_file, creation_times)
+#         if not image_predictions['Detected_items']:
+#             image_prediction_empty = True
+#         else:
+#             image_prediction_empty = False
     
-    if audio_file is not None:
+#     if audio_file is not None:
         
-        try:
-            audio_contents = await audio_file.read()
+#         try:
+#             audio_contents = await audio_file.read()
 
-            if is_audio_file(audio_file.filename):
-                audio_predictions = speechToText_model(audio_file, audio_contents)
+#             if is_audio_file(audio_file.filename):
+#                 audio_predictions = speechToText_model(audio_file, audio_contents)
                 
-                #audio_predictions = '{"Item":"test item"}'
-                audio_file_error = False
-            else:
-                audio_file_error = True
+#                 #audio_predictions = '{"Item":"test item"}'
+#                 audio_file_error = False
+#             else:
+#                 audio_file_error = True
+#         except Exception as e:
+#             audio_file_error = True
+#     else:
+#         audio_file_error = False
+#         audio_predictions = None
+        
+
+#     if image_file_error:
+#         raise HTTPException(status_code=400, detail="Image file error: The supported image file formats are '.JPEG', '.PNG', '.HEIC' and 'WebP'. ")
+#     if image_prediction_empty:
+#         raise HTTPException(status_code=200, detail="No valid object detected: Please ensure a valid item is present in the image. Valid items include 'Doors', 'Windows', 'Sinks', 'Urinals', 'Ceiling Panals','Floor Tiles', 'Water Closets' and 'Partitions'.")
+#     if audio_file_error:
+#         raise HTTPException(status_code=400, detail="Audio file error: The supported audio file formats are '.mp3', '.wav' and '.ogg'.")
+
+    
+#     response_data = {
+#         'Result':{
+#             "Image": image_predictions,
+#             "Audio": {  "Audio_available":"No",
+#                         "Item": "",
+#                         "Type": "",
+#                         "Measurement": "",
+#                         "Price": "",
+#                         "Description": ""
+#             }
+#         }
+#     }
+#     if audio_predictions:
+#         audio_predictions = json.loads(audio_predictions) 
+        
+#         if audio_file is None:
+#             response_data['Result']['Audio']["Audio_available"] = 'No'
+#         else:
+#             response_data['Result']['Audio']["Audio_available"] = 'Yes'
+#         response_data['Result']['Audio']["Item"] = audio_predictions.get("Item","")
+#         response_data['Result']['Audio']["Type"] = audio_predictions.get("Type", "")
+#         response_data['Result']['Audio']["Measurement"] = audio_predictions.get("Measurement", "")
+#         response_data['Result']['Audio']["Price"] = audio_predictions.get("Price", "")
+#         response_data['Result']['Audio']["Description"] = audio_predictions.get("Description", "")
+    
+#     return DictOut(**response_data)
+
+
+from typing import List
+
+@app.post("/predict", response_model=DictOut)
+async def upload_images(images: List[UploadFile] = File(...), audio_file: UploadFile = File(None)):
+
+    image_results = []
+    
+    for idx, image_file in enumerate(images):
+        contents = await image_file.read()
+        register_heif_opener()
+
+        try:
+            img = Image.open(io.BytesIO(contents))
         except Exception as e:
+            image_file_error = True
+        else:
+            image_file_error = False
+
+        if not image_file_error:
+            image_predictions, image_relative_path = predict_pipeline(img, contents)
+            delete_old_subdirectories(creation_times_file, creation_times)
+            if not image_predictions['Detected_items']:
+                image_prediction_empty = True
+            else:
+                image_prediction_empty = False
+
+            # Append the image results to the list
+            image_results.append({
+                f"Image_{idx+1}": image_predictions
+                
+                
+            })
+
+    if image_file_error:
+        raise HTTPException(status_code=400, detail="Image file error: The supported image file formats are '.JPEG', '.PNG', '.HEIC' and 'WebP'. ")
+    if image_prediction_empty:
+        raise HTTPException(status_code=200, detail="No valid object detected: Please ensure a valid item is present in the image. Valid items include 'Doors', 'Windows', 'Sinks', 'Urinals', 'Ceiling Panals','Floor Tiles', 'Water Closets' and 'Partitions'.")
+    
+    response_data = {
+            'Result':{
+                "Image": image_results,
+                "Description": generate_item_description(class_counts),
+                "Audio": {  "Audio_available":"No",
+                            "Item": "",
+                            "Type": "",
+                            "Measurement": "",
+                            "Price": "",
+                            "Available From": "",
+                            "Available To": "",
+                            "Location": "",
+                            "Summary": ""
+                }
+            }
+        }
+    
+    
+
+    if audio_file is not None:
+    
+       # try:
+        audio_contents = await audio_file.read()
+
+        if is_audio_file(audio_file.filename):
+            audio_predictions = speechToText_model(audio_file, audio_contents)
+            
+            #audio_predictions = '{"Item":"test item"}'
+            audio_file_error = False
+        else:
             audio_file_error = True
+        # except Exception as e:
+        #     print('Masla yahan ha 2')
+        #     audio_file_error = True
     else:
+        
         audio_file_error = False
         audio_predictions = None
         
 
-    if image_file_error:
-        raise HTTPException(status_code=400, detail="Image error: The uploaded file is not a valid image file")
-    if image_prediction_empty:
-        raise HTTPException(status_code=400, detail="No object detected, Please take a better photo.")
     if audio_file_error:
-        raise HTTPException(status_code=400, detail="Audio error: The uploaded file is not a valid audio file.")
+        raise HTTPException(status_code=400, detail="Audio file error: The supported audio file formats are '.mp3', '.wav' and '.ogg'.")
 
     
-    response_data = {
-        'Result':{
-            "Image": image_predictions,
-            "Audio": {  "Audio_available":"No",
-                        "Item": "",
-                        "Type": "",
-                        "Measurement": "",
-                        "Price": "",
-                        "Description": ""
-            }
-        }
-    }
+    
     if audio_predictions:
         audio_predictions = json.loads(audio_predictions) 
         
@@ -105,9 +207,14 @@ async def upload_image(image_file: UploadFile = File(...), audio_file: UploadFil
         response_data['Result']['Audio']["Type"] = audio_predictions.get("Type", "")
         response_data['Result']['Audio']["Measurement"] = audio_predictions.get("Measurement", "")
         response_data['Result']['Audio']["Price"] = audio_predictions.get("Price", "")
-        response_data['Result']['Audio']["Description"] = audio_predictions.get("Description", "")
+        response_data['Result']['Audio']["Available From"] = audio_predictions.get("Available From", "")
+        response_data['Result']['Audio']["Available To"] = audio_predictions.get("Available To", "")
+        response_data['Result']['Audio']["Location"] = audio_predictions.get("Location", "")
+        response_data['Result']['Audio']["Summary"] = audio_predictions.get("Summary", "")
     
+    delete_directory('audio_files')
     return DictOut(**response_data)
+
 
 
 
--- app/speechToText_model.py
+++ app/speechToText_model.py
@@ -1,43 +1,8 @@
 
 import openai
 import os
-from app.helper_functions import create_directory, delete_directory
+from app.helper_functions import create_directory, process_audio_file
 
-
-openai.api_key = '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'
-
-def process_audio_file(temperature, audio_file):
-    system_prompt =  """You have been tasked with refining the accuracy of transcribed text related to a list of items and their details. Your primary responsibilities include:\
-                    Rectifying any spelling errors in specific terms such as "Door," "Water Closet," "Partitions," "Window," "Urinal," "Ceiling Panels," "Floor Tiles," and "Pounds."\
-                    Identifying and extract the following attributes from the corrected text: \
-                    Item \
-                    Type \
-                    Measurement \
-                    Price\
-                    Description\
-                    Your process should follow these guidelines:\
-                    Correct any spelling discrepancies in the text.\
-                    Extract the aforementioned attributes from the text keeping the attributes exact as above.\
-                    If the Price is mentioned, convert it to numeric; otherwise, label it as "not mentioned."\
-                    If a Measurement is specified, convert it to numeric with suitable units; otherwise, mark it as "not mentioned."\
-                    Provide a concise two-line description for Description attribute\
-                    The final output should be formatted in JSON format."""
-    
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0301",
-        temperature=temperature,
-        messages=[
-            {
-                "role": "system",
-                "content": system_prompt
-            },
-            {
-                "role": "user",
-                "content": openai.Audio.transcribe("whisper-1", audio_file).text
-            }
-        ]
-    )
-    return response['choices'][0]['message']['content']
 
 
 def speechToText_model(audio, audio_contents):
@@ -55,6 +20,5 @@ def speechToText_model(audio, audio_contents):
     audio_file= open(audio_file_path, "rb")
     # result = openai.Audio.transcribe("whisper-1", audio_file)
     result= process_audio_file(0.7, audio_file)
-    delete_directory('audio_files')
     return result
 
